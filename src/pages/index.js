6bb637a375bf3a2efda797e891178534ba3a20f5
jaslenecodes <90170752+jaslenecodes@users.noreply.github.com>
2021-09-11 14:46:39 +0800

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import * as React from "react"
+// Import from a CSS file in your src
+import "../styles/index.css"
+// Import from an installed package
+import "bootstrap/dist/css/bootstrap.min.css"
+export default function HomePage() {
+  return <div>I'm styled by bootstrap & src/styles/index.css</div>
+}
